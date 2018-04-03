ca7c764e2f7340f65e148cfdb71ff0457ddf0370
Vijay jaiswal <vijayjaiswal@Vijays-MacBook-Air.local>
2018-04-03 22:28:39 +0530

    maven project

---
+++ src/main/java/com/example/demo/SattaApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SattaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SattaApplication.class, args);
+	}
+}
