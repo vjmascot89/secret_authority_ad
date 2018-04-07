0731fe1891cd6c2498d3618aacd5ac77417ad3dd
xenoflash <rudralande881@gmail.com>
2018-04-07 17:54:12 +0530

    chained initial html calls to register templates and when done call all satteri to render matches

--- ui/js/controllers.js
+++ ui/js/controllers.js
@@ -3,29 +3,47 @@ $( document ).ready(function() {
   var html_match_content = "";
   var html_players = "";
 
-  $.ajax({
-    url: 'match_name.html',
-    success: function(data) {
-        html_match_name = data;
-        register_template("match_name", html_match_name);
-    }
+  $.when(
+    $.get('match_name.html'),
+    $.get('match_content.html'),
+    $.get('players.html'),
+  ).done(function(match_name, match_content, players){
+    html_match_name = match_name[0];
+    html_match_content = match_content[0];
+    html_players = players[0];
+    register_template("match_name", html_match_name);
+    register_template("match_content", html_match_content);
+    register_template("players", html_players);
+    $.ajax({
+      url: '/satteri',
+      success: function(data) {
+          render_match(data);
+      }
+    });
   });
-
-  $.ajax({
-    url: 'match_content.html',
-    success: function(data) {
-        html_match_content = data;
-        register_template("match_content", html_match_content);
-    }
-  });
-
-  $.ajax({
-    url: 'players.html',
-    success: function(data) {
-        html_players = data;
-        register_template("players", html_players);
-    }
-  });
+  // $.ajax({
+  //   url: 'match_name.html',
+  //   success: function(data) {
+  //       html_match_name = data;
+  //       register_template("match_name", html_match_name);
+  //   }
+  // });
+  //
+  // $.ajax({
+  //   url: 'match_content.html',
+  //   success: function(data) {
+  //       html_match_content = data;
+  //       register_template("match_content", html_match_content);
+  //   }
+  // });
+  //
+  // $.ajax({
+  //   url: 'players.html',
+  //   success: function(data) {
+  //       html_players = data;
+  //       register_template("players", html_players);
+  //   }
+  // });
 
   function register_template(name, markup){
     $.template( name, markup );
