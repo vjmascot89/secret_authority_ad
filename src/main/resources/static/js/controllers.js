25e35d1efa6556391aa259e15949df6413e28c4b
xenoflash <rudralande881@gmail.com>
2018-04-09 01:22:00 +0530

    removing register_template from controllers.js

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -1,27 +1,4 @@
-$( document ).ready(function() {
-  debugger;
-  var html_match_name = "";
-  var html_match_content = "";
-  var html_players = "";
 
-  $.when(
-    $.get('match_name.html'),
-    $.get('match_content.html'),
-    $.get('players.html'),
-  ).done(function(match_name, match_content, players){
-    html_match_name = match_name[0];
-    html_match_content = match_content[0];
-    html_players = players[0];
-    register_template("match_name", html_match_name);
-    register_template("match_content", html_match_content);
-    register_template("players", html_players);
-    $.ajax({
-      url: '/satteri',
-      success: function(data) {
-          render_match(data);
-      }
-    });
-  });
   // $.ajax({
   //   url: 'match_name.html',
   //   success: function(data) {
