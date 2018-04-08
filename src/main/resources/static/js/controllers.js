0dc2de5944a84c3da59882cb5e05fc99df664c4c
xenoflash <rudralande881@gmail.com>
2018-04-09 01:03:20 +0530

    js functions made available

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -45,110 +45,112 @@ $( document ).ready(function() {
   //   }
   // });
 
-  function register_template(name, markup){
-    $.template( name, markup );
-  }
 
-  function match_add(){
-      debugger;
-      if (is_match_valid()) {
-          var formObj = getFormObj("start-match");
-          var data = {};
-          data["name"] = formObj.name;
-          data["balancePool"] = formObj.balancePool;
-          data["currentMatch"] = {
-            "date" : formObj.date,
-            "firstTeam" :
-              {
-                "teamName" : formObj.team1,
-                "ratio" : formObj.ratio1,
-              },
-              "secondTeam":{
-                "teamName" : formObj.team2,
-                "ratio" : formObj.ratio2,
-              }
-          };
-
-          $.post(
-            "/startmatch",
-            data,
-            function(data, status){
-              console.log(status);
-              render_match(data);
-            }
-          );
-      }
-  }
-
-  function player_add(e){
-      // debugger;
-      var matchId = e.target.value;
-      if (is_player_valid()) {
-          var formObj = getFormObj("player-add-"+matchId);
-          $.post(
-            "/sattalagao/"+matchId,
-            formObj,
-            function(data, status){
-              console.log(status);
-              render_player(data);
-            }
-          );
-      }
-  }
-
-  function getFormObj(formId) {
-      var formObj = {};
-      var inputs = $('#'+formId).serializeArray();
-      $.each(inputs, function (i, input) {
-          formObj[input.name] = input.value;
-      });
-      return formObj;
-  }
-
-  function is_match_valid(){
-    return true;
-  }
-  function is_player_valid(){
-    return true;
-  }
-
-  function render_match(matches){
-    var matches = [];
-    for (data in matches){
-      var match = {};
-      match["match_id"] = data.id;
-      match["balancePool"] = data.balancePool;
-      match["name"] = data.name;
-      match["totalBalanceOnTeamOneWin"] = data.totalBalanceOnTeamOneWin;
-      match["totalBalanceOnTeamTwoWin"] = data.totalBalanceOnTeamTwoWin;
-      match["date"] = data.currentMatch.date;
-      match["matchStatus"] = data.currentMatch.matchStatus;
-      match["team1"] = data.currentMatch.firstTeam.teamName;
-      match["team2"] = data.currentMatch.secondTeam.teamName;
-      match["ratio1"] = data.currentMatch.firstTeam.ratio;
-      match["ratio2"] = data.currentMatch.secondTeam.ratio;
-      matches.push(match);
-    }
-
-    $.tmpl( "match_name", matches ).appendTo( "#match_name" );
-    $.tmpl( "match_content", matches ).appendTo( "#match_content" );
-  }
-
-  function render_player(data){
-    var entries = [];
-    for (player in data.sattaPlayer){
-      var entry = {};
-      entry["sattaPlayerName"] = player.sattaPlayerName;
-      entry["currentPotTeamOne"] = player.currentPotTeamOne;
-      entry["currentPotRatioOnTeamOne"] = player.currentPotRatioOnTeamOne;
-      entry["teamOneWinAmount"] = player.teamOneWinAmount;
-      entry["currentPotTeamTwo"] = player.currentPotTeamTwo;
-      entry["currentPotRatioOnTeamOne"] = player.currentPotRatioOnTeamOne;
-      entry["teamTwoWinAmount"] = player.teamTwoWinAmount;
-      entry["player_id"] = player.id;
-      entries.push(match);
-    }
-
-    $.tmpl( "players", entries ).appendTo( "#players_for_match"+data.id );
-  }
 });
+
+function register_template(name, markup){
+  $.template( name, markup );
+}
+
+function match_add(){
+    debugger;
+    if (is_match_valid()) {
+        var formObj = getFormObj("start-match");
+        var data = {};
+        data["name"] = formObj.name;
+        data["balancePool"] = formObj.balancePool;
+        data["currentMatch"] = {
+          "date" : formObj.date,
+          "firstTeam" :
+            {
+              "teamName" : formObj.team1,
+              "ratio" : formObj.ratio1,
+            },
+            "secondTeam":{
+              "teamName" : formObj.team2,
+              "ratio" : formObj.ratio2,
+            }
+        };
+
+        $.post(
+          "/startmatch",
+          data,
+          function(data, status){
+            console.log(status);
+            render_match(data);
+          }
+        );
+    }
+}
+
+function player_add(e){
+    // debugger;
+    var matchId = e.target.value;
+    if (is_player_valid()) {
+        var formObj = getFormObj("player-add-"+matchId);
+        $.post(
+          "/sattalagao/"+matchId,
+          formObj,
+          function(data, status){
+            console.log(status);
+            render_player(data);
+          }
+        );
+    }
+}
+
+function getFormObj(formId) {
+    var formObj = {};
+    var inputs = $('#'+formId).serializeArray();
+    $.each(inputs, function (i, input) {
+        formObj[input.name] = input.value;
+    });
+    return formObj;
+}
+
+function is_match_valid(){
+  return true;
+}
+function is_player_valid(){
+  return true;
+}
+
+function render_match(matches){
+  var matches = [];
+  for (data in matches){
+    var match = {};
+    match["match_id"] = data.id;
+    match["balancePool"] = data.balancePool;
+    match["name"] = data.name;
+    match["totalBalanceOnTeamOneWin"] = data.totalBalanceOnTeamOneWin;
+    match["totalBalanceOnTeamTwoWin"] = data.totalBalanceOnTeamTwoWin;
+    match["date"] = data.currentMatch.date;
+    match["matchStatus"] = data.currentMatch.matchStatus;
+    match["team1"] = data.currentMatch.firstTeam.teamName;
+    match["team2"] = data.currentMatch.secondTeam.teamName;
+    match["ratio1"] = data.currentMatch.firstTeam.ratio;
+    match["ratio2"] = data.currentMatch.secondTeam.ratio;
+    matches.push(match);
+  }
+
+  $.tmpl( "match_name", matches ).appendTo( "#match_name" );
+  $.tmpl( "match_content", matches ).appendTo( "#match_content" );
+}
+
+function render_player(data){
+  var entries = [];
+  for (player in data.sattaPlayer){
+    var entry = {};
+    entry["sattaPlayerName"] = player.sattaPlayerName;
+    entry["currentPotTeamOne"] = player.currentPotTeamOne;
+    entry["currentPotRatioOnTeamOne"] = player.currentPotRatioOnTeamOne;
+    entry["teamOneWinAmount"] = player.teamOneWinAmount;
+    entry["currentPotTeamTwo"] = player.currentPotTeamTwo;
+    entry["currentPotRatioOnTeamOne"] = player.currentPotRatioOnTeamOne;
+    entry["teamTwoWinAmount"] = player.teamTwoWinAmount;
+    entry["player_id"] = player.id;
+    entries.push(match);
+  }
+
+  $.tmpl( "players", entries ).appendTo( "#players_for_match"+data.id );
+}
