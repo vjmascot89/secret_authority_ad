4f8b6b061a86d9aa05e1c76c6c0e798344f4d27f
vijayjaiswal <jaiswal.vijay1989@gmail.com>
2018-04-12 03:26:15 +0530

    refactor

--- src/main/java/com/trending/game/controllers/SattaMatchTeamsController.java
+++ src/main/java/com/trending/game/controllers/SattaMatchTeamsController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/trending/game/validator/FunctionResult.java
+++ src/main/java/com/trending/game/validator/FunctionResult.java
@@ -6,16 +6,13 @@ public class FunctionResult implements IFunctionResult{
 	
 	private String errMsg;
 	private HttpStatus status;
-	private IFunctionResult functionResult;
 
 	public FunctionResult(String errMsg , HttpStatus status) {
 		this.errMsg = errMsg;
 		this.status = status;
 		// TODO Auto-generated constructor stub
 	}
-	public FunctionResult(IFunctionResult functionResult) {
-		this.functionResult = functionResult;
-	}
+
 	@Override
 	public String getErrorMsg() {
 		// TODO Auto-generated method stub
